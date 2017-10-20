c33c94824fbaca767d8d15838d4e9e81b413812d
Jovanny02 <32178261+Jovanny02@users.noreply.github.com>
2017-10-20 03:29:11 -0400

    Something is whack in the frequency array

--- src/CharSorter.java
+++ src/CharSorter.java
@@ -2,25 +2,28 @@ import java.util.*;
 public class CharSorter {
 
     public static void main(String[] args) {
+
     Scanner scnr = new Scanner(System.in);
     boolean loopingMenu = true; // boolean for loop
-    int switchMenu = 0; // variable used for menu options
-    int frequencyInt = 0;
-    int[] charFrequencies = new int [4];
+    int switchMenu; // variable used for menu options
+    int[] charFrequencies; // array that takes in the frequencies of each type
+    char[] readAlpha; // gets input from alphabetizer
+    int[] freqCounter;    // accepts return from counter
     System.out.println("Welcome to Character Sorter Program \nPlease input a string to be sorted");
 
     String userInput = scnr.nextLine();
-    //char[] sortArray = userInput.toCharArray();   // user array input
 
     while (loopingMenu) {
-       try {
+       //try {
             System.out.println("Please select the option you would like to see");
             System.out.println("1.Display character frequencies alphabetically \n2.Display sorted frequencies \n3.Show types of character frequencies \n4.Exit");
             switchMenu = scnr.nextInt();
             switch (switchMenu) {
                 case 1:
                     System.out.println("Case 1 worked");
-                    System.out.println(alphabetizer(userInput));
+                    readAlpha = (alphabetizer(userInput));
+                    freqCounter = frequencyCounter(readAlpha);
+                    System.out.println(freqCounter[1]);
                     break;
                 case 2:
                     System.out.println("Case 2 worked");
@@ -40,23 +43,20 @@ public class CharSorter {
                     loopingMenu = true;
                     System.out.println("Error, bad input, please enter an integer value between 1-4\n");
             }
-        }
+        /*}
         catch (Exception InputMismatchException) {
             System.out.println("Error, bad input, please enter an integer value between 1-4\n");
             scnr.nextLine();
-        }
-
+        }*/
     }
 
-
-        return;
     }
 
     public static int[] charTypes(String userInput) {
         int[] returnThis = new int[4];
         char[] sortArray = userInput.toCharArray();
         for(int i = 0; i < userInput.length(); i++) {
-            if( Character.valueOf(sortArray[i]) >= 65 && Character.valueOf(sortArray[i]) <= 90 || Character.valueOf(sortArray[i]) >= 97 && Character.valueOf(sortArray[i]) <= 122) {
+            if( Character.valueOf(sortArray [i]) >= 65 && Character.valueOf(sortArray[i]) <= 90 || Character.valueOf(sortArray[i]) >= 97 && Character.valueOf(sortArray[i]) <= 122) {
                 returnThis[0]++; // Textual character counter
             }
             else if ( Character.valueOf(sortArray[i]) >= 48 && Character.valueOf(sortArray[i]) <= 57){
@@ -87,6 +87,22 @@ public class CharSorter {
 
             }
         return alphaArray;
+    }
+
+    public static int[] frequencyCounter(char[] alphaArray){
+        int lengthVariable = alphaArray.length;
+        int i = 0;
+        int a = 0;
+        int[] fQ = new int [lengthVariable];
+        for(a = 0; a < lengthVariable; a++){
+            fQ[a] = 1;
+            for(; (i < lengthVariable) && (alphaArray[i] == alphaArray[i + 1]); i++) { // i < lengthVariable) && (alphaArray[i] == alphaArray[i + 1])
+                fQ[a] += 1;
+            }
+
+        }
+
+        return fQ;
     }
 
 
